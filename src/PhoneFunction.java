5c376144a921fd1c2282492769bc7193602d11f1
phanvietdung2101 <phanvietdung2101@gmail.com>
2020-02-23 16:26:01 +0700

    build abstract class PhoneFunction

---
+++ src/PhoneFunction.java
@@ -0,0 +1,9 @@
+public abstract class PhoneFunction {
+    void insertPhone(String name, String phone){}
+    void removePhone(String name){}
+    void updatePhone(String name, String phone){}
+    Phone searchPhone(String name){
+    }
+    void sortPhone(){}
+
+}
